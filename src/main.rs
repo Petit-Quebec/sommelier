4bf4a5b4a63f0f74fd995db43e732d62703d488d
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-08-17 03:36:01 +0000

    application recognized by discord

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use ed25519_dalek::VerifyingKey;
+use ed25519_dalek::{Signature, Verifier, VerifyingKey, PUBLIC_KEY_LENGTH};
+use hex::decode;
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use lambda_http::http::StatusCode;
 use serde_json::json;
-use std::env;
 
 use crate::app::interactions::{InteractionRequest};
 use crate::app::handle_interaction;
@@ -26,6 +26,7 @@ fn lift_result<T, E>(res: Result<T, E>) -> Result<T, StatusCode> {
 
 fn get_param(event: &Request, key: &str) -> Result<String, StatusCode> {
 
+    tracing::info!({%key}, "accessing param");
     let query_map = event.query_string_parameters_ref();
     
     let val = query_map.and_then(|params| params.first(key));
@@ -52,6 +53,8 @@ fn build_error_response(code: StatusCode) -> Response<Body> {
 /// Takes in a json request body, returns a json response body
 fn handle_interaction_json(request_json: &str) -> Result<String, StatusCode> {
     
+    tracing::info!({ %request_json }, "Handling request json");
+    
     match serde_json::from_str::<InteractionRequest>(request_json) {
         
         Ok(interaction) => {
@@ -65,22 +68,68 @@ fn handle_interaction_json(request_json: &str) -> Result<String, StatusCode> {
     }
 }
 
+fn decode_hex_string(s: &str) -> Result<Vec<u8>, StatusCode> {
+
+    lift_result(hex::decode(s))
+}
+
 fn function_handler_helper(event: &Request) -> Result<Response<Body>, StatusCode> {
     
-    let application_public_key = lift_result(env::var("NYOOMIO_PUBLIC_KEY"))?;
+    tracing::info!("Got to handler helper");
     
-    let timestamp = get_param(event, "X-Signature-Ed25519")?;
+    let application_public_key_str = env!("NYOOMIO_PUBLIC_KEY");
 
-    let signature = get_param(event, "X-Signature-Timestamp")?;
-   
+    tracing::info!({%application_public_key_str}, "got public key");
+
+    let apk = decode_hex_string(&application_public_key_str)?;
+
+    tracing::info!("Decoded hex string!");
+
+    let application_public_key: [u8; PUBLIC_KEY_LENGTH] = 
+        lift_result(apk.try_into())?;
+
+    tracing::info!("application public key");
+    let verifier = lift_result(VerifyingKey::from_bytes(&application_public_key))?;
+    
+    tracing::info!("CCC");
+    let timestamp_val = lift_option(event.headers().get("X-Signature-Timestamp"))?;
+    let timestamp = lift_result(timestamp_val.to_str())?;
+
+    tracing::info!("DDD");
+    let signature_val = lift_option(event.headers().get("X-Signature-Ed25519"))?;
+    let signature = lift_result(signature_val.to_str())?;
+
+    tracing::info!({ %signature }, "Got signature");
+    tracing::info!("EEE");
+    let sb = lift_result(decode_hex_string(&signature))?;
+
+    let signature_bytes: [u8; 64] = 
+        lift_result(sb.try_into())?;
+
+    tracing::info!("FFF");
     let body = lift_result(std::str::from_utf8(event.body()))?;
 
-    let result_json = handle_interaction_json(body)?;
+    tracing::info!({ %body }, "Handling request json");
+    tracing::info!({ %signature }, "Got signature");
+    
+    let msg = (timestamp.to_owned() + body).into_bytes();
 
-    Ok(build_interaction_response(result_json))
+    match verifier.verify(&msg, &Signature::from_bytes(&signature_bytes)) {
+
+        Ok(()) => {
+
+            let result_json = handle_interaction_json(body)?;
+
+            Ok(build_interaction_response(result_json))
+        },
+
+        Err(_) => Err(StatusCode::UNAUTHORIZED)
+    }
 }
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+
+    tracing::info!("HANDLING INCOMING EVENT");
 
     match function_handler_helper(&event) {
 
