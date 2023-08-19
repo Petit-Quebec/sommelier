9c6431eea50967a8684f77794886992ba2cb8149
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-08-19 17:21:47 +0000

    refactor to use result traits

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,12 @@
 use ed25519_dalek::{Signature, Verifier, VerifyingKey, PUBLIC_KEY_LENGTH};
-use hex::decode;
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use lambda_http::http::StatusCode;
 use serde_json::json;
 
-use crate::app::interactions::{InteractionRequest};
+use crate::app::interactions::InteractionRequest;
 use crate::app::handle_interaction;
 
 mod app;
-
-fn lift_option<T>(opt: Option<T>) -> Result<T, StatusCode> {
-
-    match opt {
-
-        Some(val) => Ok(val),
-
-        None => Err(StatusCode::INTERNAL_SERVER_ERROR)
-    }
-}
-
-fn lift_result<T, E>(res: Result<T, E>) -> Result<T, StatusCode> {
-
-    lift_option(res.ok())
-}
-
-fn get_param(event: &Request, key: &str) -> Result<String, StatusCode> {
-
-    tracing::info!({%key}, "accessing param");
-    let query_map = event.query_string_parameters_ref();
-    
-    let val = query_map.and_then(|params| params.first(key));
-
-    lift_option(val.map(|s| s.to_string()))
-}
-
-fn build_interaction_response(response: String) -> Response<Body> {
-    
-    Response::builder()
-        .status(StatusCode::OK)
-        .body(response.into())
-        .unwrap()
-}
-
-fn build_error_response(code: StatusCode) -> Response<Body> {
-    
-    Response::builder()
-        .status(code)
-        .body("Error when handling request.".into())
-        .unwrap()
-}
 
 /// Takes in a json request body, returns a json response body
 fn handle_interaction_json(request_json: &str) -> Result<String, StatusCode> {
@@ -68,60 +26,46 @@ fn handle_interaction_json(request_json: &str) -> Result<String, StatusCode> {
     }
 }
 
-fn decode_hex_string(s: &str) -> Result<Vec<u8>, StatusCode> {
-
-    lift_result(hex::decode(s))
-}
-
-fn function_handler_helper(event: &Request) -> Result<Response<Body>, StatusCode> {
+fn function_handler_helper(event: &Request) -> Result<String, StatusCode> {
     
-    tracing::info!("Got to handler helper");
-    
-    let application_public_key_str = env!("NYOOMIO_PUBLIC_KEY");
-
-    tracing::info!({%application_public_key_str}, "got public key");
-
-    let apk = decode_hex_string(&application_public_key_str)?;
-
-    tracing::info!("Decoded hex string!");
-
     let application_public_key: [u8; PUBLIC_KEY_LENGTH] = 
-        lift_result(apk.try_into())?;
+        hex::decode(&env!("NYOOMIO_PUBLIC_KEY"))
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .try_into()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    tracing::info!("application public key");
-    let verifier = lift_result(VerifyingKey::from_bytes(&application_public_key))?;
+    let verifier = 
+        VerifyingKey::from_bytes(&application_public_key)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
     
-    tracing::info!("CCC");
-    let timestamp_val = lift_option(event.headers().get("X-Signature-Timestamp"))?;
-    let timestamp = lift_result(timestamp_val.to_str())?;
+    let timestamp = 
+        event.headers()
+        .get("X-Signature-Timestamp")
+        .ok_or(StatusCode::BAD_REQUEST)?
+        .to_str()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
 
-    tracing::info!("DDD");
-    let signature_val = lift_option(event.headers().get("X-Signature-Ed25519"))?;
-    let signature = lift_result(signature_val.to_str())?;
+    let signature_str = 
+        event.headers()
+        .get("X-Signature-Ed25519").ok_or(StatusCode::BAD_REQUEST)?
+        .to_str().map_err(|_| StatusCode::BAD_REQUEST)?;
 
-    tracing::info!({ %signature }, "Got signature");
-    tracing::info!("EEE");
-    let sb = lift_result(decode_hex_string(&signature))?;
+    let signature: [u8; 64] =
+        hex::decode(&signature_str)
+        .map_err(|_| StatusCode::BAD_REQUEST)?
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
 
-    let signature_bytes: [u8; 64] = 
-        lift_result(sb.try_into())?;
-
-    tracing::info!("FFF");
-    let body = lift_result(std::str::from_utf8(event.body()))?;
+    let body = std::str::from_utf8(event.body())
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
 
     tracing::info!({ %body }, "Handling request json");
-    tracing::info!({ %signature }, "Got signature");
     
     let msg = (timestamp.to_owned() + body).into_bytes();
 
-    match verifier.verify(&msg, &Signature::from_bytes(&signature_bytes)) {
+    match verifier.verify(&msg, &Signature::from_bytes(&signature)) {
 
-        Ok(()) => {
-
-            let result_json = handle_interaction_json(body)?;
-
-            Ok(build_interaction_response(result_json))
-        },
+        Ok(()) => Ok(handle_interaction_json(body)?),
 
         Err(_) => Err(StatusCode::UNAUTHORIZED)
     }
@@ -129,13 +73,21 @@ fn function_handler_helper(event: &Request) -> Result<Response<Body>, StatusCode
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 
-    tracing::info!("HANDLING INCOMING EVENT");
-
     match function_handler_helper(&event) {
 
-        Ok(result) => Ok(result),
+        Ok(response) => Ok(
+            Response::builder()
+                .status(StatusCode::OK)
+                .body(response.into())
+                .unwrap()
+        ),
 
-        Err(code) => Ok(build_error_response(code))
+        Err(code) => Ok(
+            Response::builder()
+                .status(code)
+                .body("Error when handling request.".into())
+                .unwrap()
+        )
     }
 }
 
