3cafca57f74d006beb70e42aa702aec61c2f0f2a
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-09-03 00:00:27 +0000

    interaction type enums

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,9 @@
-use crate::app::interactions::{InteractionRequest, InteractionResponse};
+use crate::app::interactions::{InteractionRequest, InteractionResponse, InteractionType, InteractionCallbackType};
 
 pub mod interactions;
 
 pub fn handle_interaction(request: &InteractionRequest) -> InteractionResponse {
     match request.r#type {
-        _ => InteractionResponse{ r#type: 1 }
+        _ => InteractionResponse{ r#type: InteractionCallbackType::Pong }
     }
 }
--- src/app/interactions.rs
+++ src/app/interactions.rs
@@ -1,14 +1,37 @@
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+#[derive(Deserialize_repr)]
+#[repr(u8)]
+pub enum InteractionType {
+    Ping = 1,
+    ApplicationCommand = 2,
+    MessageComponent = 3,
+    ApplicationCommandAutocomplete = 4,
+    ModalSubmit = 5
+}
+
+#[derive(Serialize_repr)]
+#[repr(u8)]
+pub enum InteractionCallbackType {
+    Pong = 1,
+    ChannelMessageWithSource = 4,
+    DeferredChannelMessageWithSource = 5,
+    DefferedUpdateMessage = 6,
+    UpdateMessage = 7,
+    ApplicationCommandAutocompleteResult = 8,
+    Modal = 9
+}
 
 #[derive(Deserialize)]
 pub struct InteractionRequest {
     pub id: String,
     pub application_id: String,
-    pub r#type: u32
+    pub r#type: InteractionType
 }
 
 #[derive(Serialize)]
 pub struct InteractionResponse {
-    pub r#type: u32
+    pub r#type: InteractionCallbackType
 }
 
