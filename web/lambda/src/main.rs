025b9ca347218bb9a07d6d8c6ca797f4dd7b4431
Adam Reynolds <adam.chiu.reynolds@gmail.com>
2023-09-23 21:28:45 +0000

    fixes lambda test

--- web/lambda/src/main.rs
+++ web/lambda/src/main.rs
@@ -125,7 +125,7 @@ mod tests {
         }
         "#;
 
-        let expected_pong_json = r#"{"data":null,"type":1}"#;
+        let expected_pong_json = r#"{"data":{"components":[],"content":null,"flags":64},"type":1}"#;
 
         let pong_json = handle_interaction_json(ping_json);
 
